5a8125a745a0018ac671ad30f51fd7afc9fec38f
toothlessdev <toothless042@gmail.com>
2025-06-12 20:36:24 +0900

    chore: 저장소 변경

--- cms-app/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import { router } from "@/app/Router";
-import { RouterProvider } from "react-router-dom";
-
-export default function App() {
-    return <RouterProvider router={router} />;
-}
--- cms-app/src/pages/dashboard/GuestbookPage.tsx
+++
@@ -1,3 +0,0 @@
-export default function GuestbookPage() {
-    return <div>guestbook management page</div>;
-}
